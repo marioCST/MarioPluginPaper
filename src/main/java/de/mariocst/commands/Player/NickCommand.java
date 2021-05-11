5eee667fed3097636ceadd642f728bc502f21afa
marioCST <marioCST@noreply.github.com>
2021-05-11 08:29:21 +0200

    You cannot fake anyone and you cannot use a nick that is used

--- src/main/java/de/mariocst/commands/Player/NickCommand.java
+++ src/main/java/de/mariocst/commands/Player/NickCommand.java
@@ -21,11 +21,24 @@ public class NickCommand implements CommandExecutor {
         if(player.hasPermission("mario.nick") || player.hasPermission("*") || player.isOp()) {
             if (args.length == 1) {
                 if (args[0].length() < 16) {
-                    String newNick = String.join(" ",  args);
-                    player.setDisplayName(newNick);
-                    player.setPlayerListName(newNick);
-                    player.setCustomName(newNick);
-                    sender.sendMessage(MarioMain.PREFIX + "Dein Nickname wurde erfolgreich zu " + newNick + " geändert!");
+                    for (Player t : MarioMain.getInstance().getServer().getOnlinePlayers()) {
+                        if (args[0].equals(t.getName()) && !args[0].equals(player.getName())) {
+                            sender.sendMessage(MarioMain.PREFIX + "Bitte ''fake'' keine Spieler!");
+                        }
+                        else if (args[0].equals(player.getName())) {
+                            sender.sendMessage(MarioMain.PREFIX + "/unnick");
+                        }
+                        else if (args[0].equals(t.getCustomName())) {
+                            sender.sendMessage(MarioMain.PREFIX + "Jemand hat sich bereits " + args[0] + " genannt!");
+                        }
+                        else {
+                            String newNick = String.join(" ",  args);
+                            player.setDisplayName(newNick);
+                            player.setPlayerListName(newNick);
+                            player.setCustomName(newNick);
+                            sender.sendMessage(MarioMain.PREFIX + "Dein Nickname wurde erfolgreich zu " + newNick + " geändert!");
+                        }
+                    }
                 } else {
                     sender.sendMessage(MarioMain.PREFIX + "Bitte wähle einen Namen, der kürzer als 16 Zeichen ist!");
                     player.playSound(player.getLocation(), Sound.BLOCK_ANVIL_LAND, 0.2f, 1.2f);
