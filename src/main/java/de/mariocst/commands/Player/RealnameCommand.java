3ec761f88904030d8500267361bfb092681f780c
marioCST <marioCST@noreply.github.com>
2021-07-02 21:04:05 +0200

    Fixed /realname

--- src/main/java/de/mariocst/commands/Player/RealnameCommand.java
+++ src/main/java/de/mariocst/commands/Player/RealnameCommand.java
@@ -11,42 +11,50 @@ public class RealnameCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if(!(sender instanceof Player)) {
-            MarioMain.getInstance().log("InDev");
+            if (args.length == 1) {
+                for (Player online : MarioMain.getInstance().getServer().getOnlinePlayers()) {
+                    if (online.getDisplayName().equals(args[0])) {
+                        if (!online.getDisplayName().equals(online.getName())) {
+                            sender.sendMessage(MarioMain.getPrefix() + "Der echte Name von " + args[0] + " ist: " + online.getName());
+                        }
+                        else {
+                            sender.sendMessage(MarioMain.getPrefix() + "Dieser Spieler ist nicht genickt!");
+                        }
+                    }
+                    else {
+                        sender.sendMessage(MarioMain.getPrefix() + "Dieser Spieler Nick existiert nicht!");
+                    }
+                }
+            } else if (args.length == 0) {
+                sender.sendMessage(MarioMain.getPrefix() + "Bitte gib den Nickname an.");
+            } else {
+                sender.sendMessage(MarioMain.getPrefix() + "§cBitte wähle einen gültigen Namen!");
+            }
         } else {
             Player player = (Player) sender;
 
             if (player.hasPermission("mario.realname") || player.isOp()) {
-                if (player.getName().equals("marioCST")) {
-                    String target = args[0];
-                    Player t = MarioMain.getInstance().getServer().getPlayer(target);
-
-                    if (args.length == 1) {
-                        try {
-                            if (null != t) {
-                                if (t.getName().equals(args[0])) {
-                                    sender.sendMessage(MarioMain.getPrefix() + "Dieser Spieler ist NICHT genickt!");
-                                    player.playSound(player.getLocation(), Sound.BLOCK_ANVIL_LAND, 1.0F, 1.0F);
-                                } else {
-                                    sender.sendMessage(MarioMain.getPrefix() + "Der Echte Name von " + args[0] + " ist: " + t.getName());
-                                }
-                            } else  {
-                                sender.sendMessage(MarioMain.getPrefix() + "java.lang.NullPointerException lol");
+                if (args.length == 1) {
+                    for (Player online : MarioMain.getInstance().getServer().getOnlinePlayers()) {
+                        if (online.getDisplayName().equals(args[0])) {
+                            if (!online.getDisplayName().equals(online.getName())) {
+                                sender.sendMessage(MarioMain.getPrefix() + "Der echte Name von " + args[0] + " ist: " + online.getName());
+                            }
+                            else {
+                                sender.sendMessage(MarioMain.getPrefix() + "Dieser Spieler ist nicht genickt!");
                                 player.playSound(player.getLocation(), Sound.BLOCK_ANVIL_LAND, 1.0F, 1.0F);
                             }
-                        } catch (NullPointerException e) {
-                            sender.sendMessage(MarioMain.getPrefix() + "java.lang.NullPointerException lol");
-                            player.playSound(player.getLocation(), Sound.BLOCK_ANVIL_LAND, 1.0F, 1.0F);
-                            e.printStackTrace();
                         }
-                    } else if (args.length == 0) {
-                        sender.sendMessage(MarioMain.getPrefix() + "Bitte gib den Nickname an.");
-                        player.playSound(player.getLocation(), Sound.BLOCK_ANVIL_LAND, 1.0F, 1.0F);
-                    } else {
-                        sender.sendMessage(MarioMain.getPrefix() + "§cBitte wähle einen gültigen Namen!");
-                        player.playSound(player.getLocation(), Sound.BLOCK_ANVIL_LAND, 1.0F, 1.0F);
+                        else {
+                            sender.sendMessage(MarioMain.getPrefix() + "Dieser Spieler Nick existiert nicht!");
+                            player.playSound(player.getLocation(), Sound.BLOCK_ANVIL_LAND, 1.0F, 1.0F);
+                        }
                     }
+                } else if (args.length == 0) {
+                    sender.sendMessage(MarioMain.getPrefix() + "Bitte gib den Nickname an.");
+                    player.playSound(player.getLocation(), Sound.BLOCK_ANVIL_LAND, 1.0F, 1.0F);
                 } else {
-                    sender.sendMessage(MarioMain.getPrefix() + "InDev");
+                    sender.sendMessage(MarioMain.getPrefix() + "§cBitte wähle einen gültigen Namen!");
                     player.playSound(player.getLocation(), Sound.BLOCK_ANVIL_LAND, 1.0F, 1.0F);
                 }
             } else {
