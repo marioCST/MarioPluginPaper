43a346bd96f1558449ce83bcaaa5ab0197f78a8d
marioCST <marioCST@noreply.github.com>
2021-08-08 18:25:22 +0200

    Added thunderstrike and drop troll, remade explosion troll

--- src/main/java/de/mariocst/commands/Others/TrollCommand.java
+++ src/main/java/de/mariocst/commands/Others/TrollCommand.java
@@ -10,35 +10,41 @@ import org.bukkit.command.CommandSender;
 
 import org.bukkit.craftbukkit.v1_16_R3.entity.CraftPlayer;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
-
-import java.util.Collections;
 
 public class TrollCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if(!(sender instanceof Player)) {
             try {
-                if (args.length == 2) {
+                if (args.length >= 2) {
                     Player t = MarioMain.getInstance().getServer().getPlayer(args[1]);
 
                     try {
                         if (t != null) {
                             switch (args[0].toLowerCase()) {
                                 case "explosion" -> {
-                                    ((CraftPlayer)t).getHandle().playerConnection.sendPacket(new PacketPlayOutExplosion(
-                                            t.getLocation().getX(),
-                                            t.getLocation().getY(),
-                                            t.getLocation().getZ(),
-                                            5.0F,
-                                            Collections.EMPTY_LIST,
-                                            new Vec3D(
-                                                    t.getLocation().getBlockX(),
-                                                    t.getLocation().getBlockY(),
-                                                    t.getLocation().getBlockZ()
-                                            )));
+                                    if (args.length == 3) {
+                                        float multiplier = Float.parseFloat(args[2]);
+                                        float value = multiplier * 4.0f;
+
+                                        if (multiplier > 45.0f) {
+                                            sender.sendMessage(MarioMain.getPrefix() + "Bitte gib eine kleinere Zahl ein!");
+                                        }
+                                        else if (multiplier <= 0.0f) {
+                                            sender.sendMessage(MarioMain.getPrefix() + "Bitte gib eine größere Zahl ein!");
+                                        }
+                                        else {
+                                            t.getWorld().createExplosion(t.getLocation(), value);
+                                            sender.sendMessage(MarioMain.getPrefix() + "BOOM!");
+                                        }
+                                    }
+                                    else {
+                                        t.getWorld().createExplosion(t.getLocation(), 4.0f);
+                                    }
                                 }
                                 case "inventory", "inv" -> {
                                     if (MarioMain.getInstance().invTroll.contains(t)) {
@@ -62,8 +68,22 @@ public class TrollCommand implements CommandExecutor {
                                         }
                                     }
                                 }
+                                case "thunderstrike", "ts", "strike" -> {
+                                    t.getWorld().spawnEntity(t.getLocation(), EntityType.LIGHTNING);
+
+                                    sender.sendMessage(MarioMain.getPrefix() + "Der Spieler " + t.getName() + " hat einen Schlag!");
+                                }
+                                case "drop" -> {
+                                    for (int i = 0; i <= 39; i++) {
+                                        if (t.getInventory().getItem(i) != null) {
+                                            t.getWorld().dropItem(t.getLocation(), t.getInventory().getItem(i));
+                                        }
+                                    }
+
+                                    sender.sendMessage(MarioMain.getPrefix() + "Der Spieler " + t.getName() + " wurde erfolgreich mit Itemdrop getrollt!");
+                                }
                                 default -> {
-                                    sender.sendMessage(MarioMain.getPrefix() + "/troll <explosion|inventory> <Spieler>");
+                                    sender.sendMessage(MarioMain.getPrefix() + "/troll <explosion|inventory|thunderstrike|drop> <Spieler>");
                                 }
                             }
                         }
@@ -90,24 +110,33 @@ public class TrollCommand implements CommandExecutor {
         Player player = (Player) sender;
         if (player.hasPermission("mario.troll") || player.hasPermission("*") || player.isOp()) {
             try {
-                if (args.length == 2) {
+                if (args.length >= 2) {
                     Player t = MarioMain.getInstance().getServer().getPlayer(args[1]);
 
                     try {
                         if (t != null) {
                             switch (args[0].toLowerCase()) {
                                 case "explosion" -> {
-                                    ((CraftPlayer)t).getHandle().playerConnection.sendPacket(new PacketPlayOutExplosion(
-                                            t.getLocation().getX(),
-                                            t.getLocation().getY(),
-                                            t.getLocation().getZ(),
-                                            5.0F,
-                                            Collections.EMPTY_LIST,
-                                            new Vec3D(
-                                                    t.getLocation().getBlockX(),
-                                                    t.getLocation().getBlockY(),
-                                                    t.getLocation().getBlockZ()
-                                            )));
+                                    if (args.length == 3) {
+                                        float multiplier = Float.parseFloat(args[2]);
+                                        float value = multiplier * 4.0f;
+
+                                        if (multiplier > 45.0f) {
+                                            player.sendMessage(MarioMain.getPrefix() + "Bitte gib eine kleinere Zahl ein!");
+                                            player.playSound(player.getLocation(), Sound.BLOCK_ANVIL_LAND, 0.2f, 1.2f);
+                                        }
+                                        else if (multiplier <= 0.0f) {
+                                            player.sendMessage(MarioMain.getPrefix() + "Bitte gib eine größere Zahl ein!");
+                                            player.playSound(player.getLocation(), Sound.BLOCK_ANVIL_LAND, 0.2f, 1.2f);
+                                        }
+                                        else {
+                                            t.getWorld().createExplosion(t.getLocation(), value);
+                                            player.sendMessage(MarioMain.getPrefix() + "BOOM!");
+                                        }
+                                    }
+                                    else {
+                                        t.getWorld().createExplosion(t.getLocation(), 4.0f);
+                                    }
                                 }
                                 case "inventory", "inv" -> {
                                     if (MarioMain.getInstance().invTroll.contains(t)) {
@@ -121,8 +150,22 @@ public class TrollCommand implements CommandExecutor {
                                         player.sendMessage(MarioMain.getPrefix() + "Der Spieler " + t.getName() + " darf nun nicht mehr sein Inventar benutzen!");
                                     }
                                 }
+                                case "thunderstrike", "ts", "strike" -> {
+                                    t.getWorld().spawnEntity(t.getLocation(), EntityType.LIGHTNING);
+
+                                    sender.sendMessage(MarioMain.getPrefix() + "Der Spieler " + t.getName() + " hat einen Schlag!");
+                                }
+                                case "drop" -> {
+                                    for (int i = 0; i <= 39; i++) {
+                                        if (t.getInventory().getItem(i) != null) {
+                                            t.getWorld().dropItem(t.getLocation(), t.getInventory().getItem(i));
+                                        }
+                                    }
+
+                                    sender.sendMessage(MarioMain.getPrefix() + "Der Spieler " + t.getName() + " wurde erfolgreich mit Itemdrop getrollt!");
+                                }
                                 default -> {
-                                    player.sendMessage(MarioMain.getPrefix() + "/troll <explosion|inventory> <Spieler>");
+                                    player.sendMessage(MarioMain.getPrefix() + "/troll <explosion|inventory|thunderstrike|drop> <Spieler>");
                                     player.playSound(player.getLocation(), Sound.BLOCK_ANVIL_LAND, 0.2f, 1.2f);
                                 }
                             }
