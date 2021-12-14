6b3c0960362972ecbcae6eb4b8fcc37d3f44aae5
marioCST <marioCST@noreply.github.com>
2021-12-14 20:10:46 +0100

    Fix scoreboard

--- src/main/java/de/mariocst/scoreboard/MarioScoreboard.java
+++ src/main/java/de/mariocst/scoreboard/MarioScoreboard.java
@@ -29,83 +29,220 @@ public class MarioScoreboard {
         this.objective = scoreboard.registerNewObjective("display", "dummy", displayName);
         this.objective.setDisplaySlot(DisplaySlot.SIDEBAR);
 
-        this.createScoreboard();
+        if (player == null) return;
+
+        this.setDefaultScore();
         this.run();
     }
 
-    public void createScoreboard() {
-        if (this.player == null) return;
+    private void setDefaultScore() {
+        setScore("§f»»»»»»««««««", 0);
 
-        this.setScore(ChatColor.BLACK.toString(), 4);
+        if (player != null) {
+            setScore(ChatColor.BLACK.toString(), 4);
 
-        this.setScore("§7Dein Rang:", 3);
+            setScore("§7Dein Rang:", 3);
 
-        if (player.isOp()) {
-            this.setScore("§cOperator", 2);
+            if (player.isOp()) {
+                setScore("§cOperator", 2);
+            }
+            else {
+                setScore("§7Spieler", 2);
+            }
+
+            setScore(ChatColor.GOLD.toString(), 1);
         }
-        else {
-            this.setScore("§7Spieler", 2);
-        }
 
-        this.setScore(ChatColor.GOLD.toString(), 1);
+        setScore("§f»»»»»»««««««§f", 5);
     }
 
     public void setScore(String content, int score) {
         this.objective.getScore(content).setScore(score);
     }
 
+    public void editScore(String oldContent, String newContent, int score) {
+        this.objective.getScore(oldContent).resetScore();
+        this.objective.getScore(newContent).setScore(score);
+    }
+
+    public void removeScore(String content) {
+        this.objective.getScore(content).resetScore();
+    }
+
     private void run() {
         new BukkitRunnable() {
             @Override
             public void run() {
+                if (player == null) return;
+
                 switch (socialId) {
                     case 0 -> {
-                        setScore("§f»»»»»»««««««", 0);
-                        setScore("§f»»»»»»««««««", 5);
+                        editScore("§6»§f»»»»»«««««§6«§f", "§f»»»»»»««««««§f", 0);
+
+                        if (player.isOp()) {
+                            removeScore("§7Spieler");
+                            setScore("§cOperator", 2);
+                        }
+                        else {
+                            removeScore("§cOperator");
+                            setScore("§7Spieler", 2);
+                        }
+
+                        editScore("§6»§f»»»»»«««««§6«", "§f»»»»»»««««««", 5);
                     }
                     case 1 -> {
-                        setScore("§f»»»»»§6»«§f«««««", 0);
-                        setScore("§f»»»»»§6»«§f«««««", 5);
+                        editScore("§f»»»»»»««««««§f", "§f»»»»»§6»«§f«««««§f", 0);
+
+                        if (player.isOp()) {
+                            removeScore("§7Spieler");
+                            setScore("§cOperator", 2);
+                        }
+                        else {
+                            removeScore("§cOperator");
+                            setScore("§7Spieler", 2);
+                        }
+
+                        editScore("§f»»»»»»««««««", "§f»»»»»§6»«§f«««««", 5);
                     }
                     case 2 -> {
-                        setScore("§f»»»»§6»»««§f««««", 0);
-                        setScore("§f»»»»§6»»««§f««««", 5);
+                        editScore("§f»»»»»§6»«§f«««««§f", "§f»»»»§6»»««§f««««§f", 0);
+
+                        if (player.isOp()) {
+                            removeScore("§7Spieler");
+                            setScore("§cOperator", 2);
+                        }
+                        else {
+                            removeScore("§cOperator");
+                            setScore("§7Spieler", 2);
+                        }
+
+                        editScore("§f»»»»»§6»«§f«««««", "§f»»»»§6»»««§f««««", 5);
                     }
                     case 3 -> {
-                        setScore("§f»»»§6»»»«««§f«««", 0);
-                        setScore("§f»»»§6»»»«««§f«««", 5);
+                        editScore("§f»»»»§6»»««§f««««§f", "§f»»»§6»»»«««§f«««§f", 0);
+
+                        if (player.isOp()) {
+                            removeScore("§7Spieler");
+                            setScore("§cOperator", 2);
+                        }
+                        else {
+                            removeScore("§cOperator");
+                            setScore("§7Spieler", 2);
+                        }
+
+                        editScore("§f»»»»§6»»««§f««««", "§f»»»§6»»»«««§f«««", 5);
                     }
                     case 4 -> {
-                        setScore("§f»»§6»»»»««««§f««", 0);
-                        setScore("§f»»§6»»»»««««§f««", 5);
+                        editScore("§f»»»§6»»»«««§f«««§f", "§f»»§6»»»»««««§f««§f", 0);
+
+                        if (player.isOp()) {
+                            removeScore("§7Spieler");
+                            setScore("§cOperator", 2);
+                        }
+                        else {
+                            removeScore("§cOperator");
+                            setScore("§7Spieler", 2);
+                        }
+
+                        editScore("§f»»»§6»»»«««§f«««", "§f»»§6»»»»««««§f««", 5);
                     }
                     case 5 -> {
-                        setScore("§f»§6»»»»»«««««§f«", 0);
-                        setScore("§f»§6»»»»»«««««§f«", 5);
+                        editScore("§f»»§6»»»»««««§f««§f", "§f»§6»»»»»«««««§f«§f", 0);
+
+                        if (player.isOp()) {
+                            removeScore("§7Spieler");
+                            setScore("§cOperator", 2);
+                        }
+                        else {
+                            removeScore("§cOperator");
+                            setScore("§7Spieler", 2);
+                        }
+
+                        editScore("§f»»§6»»»»««««§f««", "§f»§6»»»»»«««««§f«", 5);
                     }
                     case 6 -> {
-                        setScore("§6»»»»»»««««««", 0);
-                        setScore("§6»»»»»»««««««", 5);
+                        editScore("§f»§6»»»»»«««««§f«§f", "§6»»»»»»««««««§f", 0);
+
+                        if (player.isOp()) {
+                            removeScore("§7Spieler");
+                            setScore("§cOperator", 2);
+                        }
+                        else {
+                            removeScore("§cOperator");
+                            setScore("§7Spieler", 2);
+                        }
+
+                        editScore("§f»§6»»»»»«««««§f«", "§6»»»»»»««««««", 5);
                     }
                     case 7 -> {
-                        setScore("§6»»»»»§f»«§6«««««", 0);
-                        setScore("§6»»»»»§f»«§6«««««", 5);
+                        editScore("§6»»»»»»««««««§f", "§6»»»»»§f»«§6«««««§f", 0);
+
+                        if (player.isOp()) {
+                            removeScore("§7Spieler");
+                            setScore("§cOperator", 2);
+                        }
+                        else {
+                            removeScore("§cOperator");
+                            setScore("§7Spieler", 2);
+                        }
+
+                        editScore("§6»»»»»»««««««", "§6»»»»»§f»«§6«««««", 5);
                     }
                     case 8 -> {
-                        setScore("§6»»»»§f»»««§6««««", 0);
-                        setScore("§6»»»»§f»»««§6««««", 5);
+                        editScore("§6»»»»»§f»«§6«««««§f", "§6»»»»§f»»««§6««««§f", 0);
+
+                        if (player.isOp()) {
+                            removeScore("§7Spieler");
+                            setScore("§cOperator", 2);
+                        }
+                        else {
+                            removeScore("§cOperator");
+                            setScore("§7Spieler", 2);
+                        }
+
+                        editScore("§6»»»»»§f»«§6«««««", "§6»»»»§f»»««§6««««", 5);
                     }
                     case 9 -> {
-                        setScore("§6»»»§f»»»«««§6«««", 0);
-                        setScore("§6»»»§f»»»«««§6«««", 5);
+                        editScore("§6»»»»§f»»««§6««««§f", "§6»»»§f»»»«««§6«««§f", 0);
+
+                        if (player.isOp()) {
+                            removeScore("§7Spieler");
+                            setScore("§cOperator", 2);
+                        }
+                        else {
+                            removeScore("§cOperator");
+                            setScore("§7Spieler", 2);
+                        }
+
+                        editScore("§6»»»»§f»»««§6««««", "§6»»»§f»»»«««§6«««", 5);
                     }
                     case 10 -> {
-                        setScore("§6»»§f»»»»««««§6««", 0);
-                        setScore("§6»»§f»»»»««««§6««", 5);
+                        editScore("§6»»»§f»»»«««§6«««§f", "§6»»§f»»»»««««§6««§f", 0);
+
+                        if (player.isOp()) {
+                            removeScore("§7Spieler");
+                            setScore("§cOperator", 2);
+                        }
+                        else {
+                            removeScore("§cOperator");
+                            setScore("§7Spieler", 2);
+                        }
+
+                        editScore("§6»»»§f»»»«««§6«««", "§6»»§f»»»»««««§6««", 5);
                     }
                     case 11 -> {
-                        setScore("§6»§f»»»»»«««««§6«", 0);
-                        setScore("§6»§f»»»»»«««««§6«", 5);
+                        editScore("§6»»§f»»»»««««§6««§f", "§6»§f»»»»»«««««§6«§f", 0);
+
+                        if (player.isOp()) {
+                            removeScore("§7Spieler");
+                            setScore("§cOperator", 2);
+                        }
+                        else {
+                            removeScore("§cOperator");
+                            setScore("§7Spieler", 2);
+                        }
+
+                        editScore("§6»»§f»»»»««««§6««", "§6»§f»»»»»«««««§6«", 5);
                     }
                 }
 
